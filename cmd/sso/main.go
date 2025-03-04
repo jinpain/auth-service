5ae8393712ab98142db19fd99f6ee7603e61c08b
Pavel <savpavel2000@gmail.com>
2025-03-04 19:29:41 +0500

    added logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,13 +1,43 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/HIS-Vita/auth-service/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad("config/local.yaml")
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+
+	log.Info("statring application", slog.Any("config", cfg))
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
